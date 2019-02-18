58ea03c90a104f581ec6fdedc4d2eda0e344a538
d-mich <40661164+d-mich@users.noreply.github.com>
2019-02-18 23:46:52 +0100

    cancellazione account

--- passwordkeeper/src/components/Delete.js
+++ passwordkeeper/src/components/Delete.js
@@ -21,9 +21,12 @@ class Delete extends Component {
       this.setState({ show: true });
     }
 
-    /* deleteUserData(idUser){
-      fire.database().ref('users/'+idUser).remove();
-    } */
+    deleteUserData(){
+      fire.database().ref('users/'+localStorage.getItem('userID')).remove();
+      
+      //logout
+      fire.auth().signOut()
+    }
 
     componentWillMount() {
     }
@@ -31,7 +34,11 @@ class Delete extends Component {
     render() {
       const id = localStorage.getItem('userID')
       return (
-          <div>
+          <div className="deleteComponents">
+            <Button variant="danger" onClick={this.handleShow}>
+              Cancella Account Definitivamente
+            </Button>
+
             <Modal show={this.state.show} onHide={this.handleClose}>
                 <Modal.Header closeButton>
                 <Modal.Title>Cancellazione Account</Modal.Title>
@@ -48,7 +55,7 @@ class Delete extends Component {
                 <Button variant="success" onClick={this.handleClose}>
                     Annulla
                 </Button>
-                <Button variant="danger" onClick={this.handleClose}>
+                <Button variant="danger" onClick={this.deleteUserData && this.handleClose}>
                     Elimina
                 </Button>
                 </Modal.Footer>
